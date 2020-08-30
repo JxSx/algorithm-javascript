dbf3db2e169ac1731027971fccd49a1cd89b1b5c
lisissi <845898478@qq.com>
2020-08-30 19:40:01 +0800

    ✨feat: 新增二叉搜索树的判断合法性、插入、查找算法

--- BinarySearchTree/index.js
+++ BinarySearchTree/index.js
@@ -17,6 +17,47 @@ function isValidBST(root, min, max) {
 
     return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
 }
+/**
+ * 在BST中查找一个数是否存在
+ * @param {*} root 
+ * @param {*} target 
+ */
+function isInBST(root, target) {
+    if (!root) return false;
+    console.log(root.val)
+    if (root.val === target) return true;
+    if (root.val < target) {
+        return isInBST(root.right, target);
+    }
+    if (root.val > target) {
+        return isInBST(root.left, target);
+    }
+}
 
-const tree = deserializeLevel([5, 2, 6, 1, 4, null, 7, null, null, 3])
-console.log(JSON.stringify(tree, null, 2))
+function TreeNode(val) {
+    this.val = val;
+}
+/**
+ * 在BST中插入一个数
+ * @param {*} root 
+ * @param {*} target 
+ */
+function insertInfoBST(root, target) {
+    if (!root) return new TreeNode(target);
+    if (root.val < target) {
+        root.right = insertInfoBST(root.right, target);
+    }
+    if (root.val > target) {
+        root.left = insertInfoBST(root.left, target);
+    }
+    return root;
+}
+
+if (require.main === module) {
+    const tree = deserializeLevel([5, 2, 6, 1, 4, null, 7, null, null, 3])
+    console.log(JSON.stringify(tree, null, 2))
+    console.log("合法性：", isValidBST(tree))
+    console.log("查找结点：", isInBST(tree, 3))
+    const newTree = insertInfoBST(tree, 2.2);
+    console.log("新树：", JSON.stringify(newTree, null, 2));
+}
