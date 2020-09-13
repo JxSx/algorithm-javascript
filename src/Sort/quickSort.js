93e22da7caa9d2a030db42946e49f7e722e41bab
JxSx <847114815@qq.com>
2020-09-13 23:22:33 +0800

    ✨feat: add quick sort

---
+++ src/Sort/quickSort.js
@@ -0,0 +1,38 @@
+const { data, exch } = require('./common');
+//  0  1   2   3  4   5   6   7   8   9  10, 11  12  13  14
+// [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]  索引9赋值给索引0
+// [2, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]  索引1赋值给索引9
+// [2, 3, 38, 5, 47, 15, 36, 26, 27, 44, 46, 4, 19, 50, 48]
+
+// [2, 3, 19, 5, 47, 15, 36, 26, 27, 44, 46, 4, 19, 50, 48]
+// [2, 3, 19, 5, 47, 15, 36, 26, 27, 44, 46, 4, 47, 50, 48]
+// [2, 3, 19, 5, 4, 15, 36, 26, 27, 44, 46, 4, 47, 50, 48]
+// [2, 3, 19, 5, 4, 15, 36, 26, 27, 38, 46, 44, 47, 50, 48]
+const partition = (array, low, high) => {
+    let pivot = array[low];
+    while (low < high) {
+        while (low < high && array[high] >= pivot) {
+            high--;
+        }
+        array[low] = array[high];
+        console.log(`Index:${high}的值：${array[high]}赋值给Index:${low}`);
+        while (low < high && array[low] <= pivot) {
+            low++;
+        }
+        array[high] = array[low];
+        console.log(`Index:${low}-${array[low]}赋值给Index:${high}`);
+    }
+    array[low] = pivot;
+    console.log(`基准值:${pivot}赋值给Index:${low}`);
+    return low;
+}
+
+const quickSort = (array, low, high) => {
+    if (low >= high) return;
+    let mid = partition(array, low, high);
+    quickSort(array, low, mid - 1);
+    quickSort(array, mid + 1, high)
+    return array;
+}
+
+console.log(quickSort(data, 0, data.length - 1));
